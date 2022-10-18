13426b645c4f5be46789965b251dd9179eecac77
Flávio Tomé <flaviotome5@gmail.com>
2022-10-17 23:59:02 -0300

    login

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+gem 'ransack'
 
 gem 'slack-notifier', '~> 2.4'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  #devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :employees
 
   root "employees#index"
