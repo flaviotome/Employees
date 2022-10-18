a35206aedb0f215963d6f6b87c22bfb1aa348fb3
Flávio Tomé <flaviotome5@gmail.com>
2022-10-18 10:16:51 -0300

    correção da url do slack notifier

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -31,7 +31,7 @@ class EmployeesController < ApplicationController
         format.html { redirect_to employee_url(@employee), notice: "Employee was successfully created." }
         format.json { render :show, status: :created, location: @employee }
 
-        notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+        notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
           defaults channel: "geral",
                    username: "Employer App"
         end
